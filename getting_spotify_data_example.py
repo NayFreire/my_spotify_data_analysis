f3794f4eaaff80e8b5ccf179d61d1f23d9924c41
NayFreire <nayfreireof@gmail.com>
2025-04-21 00:10:37 -0300

    Using SpotifyOAuth to get the recently played songs

--- getting_spotify_data_example.py
+++ getting_spotify_data_example.py
@@ -1,10 +1,11 @@
-from spotify_credentials import getting_spotify_credentials
+from spotify_credentials import get_sp_credentials
 
-sp = getting_spotify_credentials()
+sp = get_sp_credentials()
 
 # Ex: looking for an artist
 result = sp.search(q='Arctic Monkeys', type='artist')
 artist = result['artists']['items'][0]
+
 print(f"Name: {artist['name']}")
 print(f"Popularidade: {artist['popularity']}")
 print(f"Gêneros: {artist['genres']}")
--- spotify_credentials.py
+++ spotify_credentials.py
@@ -1,18 +1,25 @@
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
 
-def getting_spotify_credentials():
+def get_env_sp_credentials():
     # Getting the credentials from the .env file
     load_dotenv()
     client_id = os.getenv("CLIENT_ID")
     client_secret = os.getenv("CLIENT_SECRET")
+
+    return client_id, client_secret
+
+def get_sp_credentials():
+    client_id, client_secret = get_env_sp_credentials()
     
     # Setting client credentials    
-    spotify_oauth = SpotifyClientCredentials(
+    spotify_oauth = SpotifyOAuth(
         client_id=client_id,
-        client_secret=client_secret
+        client_secret=client_secret,
+        redirect_uri='http://127.0.0.1:3000/callback',
+        scope='user-read-recently-played'
     )
     
     # Instantiating the Spotify Manager    
@@ -21,5 +28,3 @@ def getting_spotify_credentials():
     )
     
     return sp
-
-
